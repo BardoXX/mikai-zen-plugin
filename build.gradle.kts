bd7608a376be2e566d53c4740da0241f86b3981b
BardoXX <88047252+BardoXX@users.noreply.github.com>
2025-10-21 10:43:12 +0200

    v1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,11 +12,12 @@ repositories {
     maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
     maven { url = uri("https://jitpack.io") }
     maven { url = uri("https://repo.maven.apache.org/maven2/") }
+    maven { url = uri("https://repo.papermc.io/repository/maven-public/") }
 }
 
 dependencies {
     compileOnly("com.github.MilkBowl:VaultAPI:1.7")
-    compileOnly("org.spigotmc:spigot-api:1.18.2-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
     compileOnly("com.github.PlaceholderAPI:PlaceholderAPI:2.10.9")
     compileOnly("net.luckperms:api:5.3")
 }
